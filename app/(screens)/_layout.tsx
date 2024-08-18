d83b6bc7427cb724fe02be8ca6eb2bbf07eea5ed
Carlos Jiménez <cajimenezca@unal.edu.co>
2024-08-18 18:03:05 -0500

    ✨ Updated tab-routing to stack-routing

---
+++ app/(screens)/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+
+// import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  // const colorScheme = useColorScheme();
+
+  return (
+    <Stack screenOptions={{
+      headerShown: false,
+    }}>
+      <Stack.Screen name="(home)" />
+      <Stack.Screen name="explore" />
+    </Stack>
+  );
+}
